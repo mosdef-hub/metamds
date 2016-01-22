da94a1dd923bb1d518298f878873ca2d0a29b6e2
Christoph Klein <christoph.t.klein@me.com>
2016-01-22 14:10:53 -0600

    Factor out rsync

--- metamds/io.py
+++ metamds/io.py
@@ -7,8 +7,29 @@ def cmd_line(line):
     args = shlex.split(line)
     proc = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = proc.communicate()
+    # Gromacs logs a lot of its informational output to stderr.
     if 'GROMACS' in err.decode('utf-8'):
         out += err
         err = b''
     return out, err
 
+
+# TODO: fix this disgustingness
+def rsync_to(flags, src, dst, user, host, logger=None):
+    cmd = 'rsync {flags} {src} {user}@{host}:{dst}'.format(**locals())
+    _rsync(cmd, logger)
+
+
+def rsync_from(flags, src, dst, user, host, logger=None):
+    cmd = 'rsync {flags} {user}@{host}:{src} {dst}'.format(**locals())
+    _rsync(cmd, logger)
+
+
+def _rsync(cmd, logger=None):
+    out, err = cmd_line(cmd)
+    if err:
+        raise IOError(err.decode('utf-8'))
+    if logger:
+        logger.debug(cmd)
+        for line in out.splitlines():
+            logger.debug(line)
